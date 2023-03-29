3680b685cf0de610e45ba6ad0eb84b2623c7eeb0
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-03-29 08:32:03 +0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_052956) do
+  create_table "hero_powers", force: :cascade do |t|
+    t.string "strength"
+    t.string "power_id"
+    t.string "hero_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "super_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "powers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
